938818ae1f517a9da1f68997f7a575f25022338d
royalejandro09 <roymartinezv@gmail.com>
2022-10-01 17:28:53 -0500

    added microservice movies

---
+++ movie-service/movie-service/src/test/java/com/ramv/movieservice/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramv.movieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
