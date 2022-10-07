83d829780286f14ba23eaf33085f63ea50d34d37
royalejandro09 <roymartinezv@gmail.com>
2022-10-06 21:35:01 -0500

    changes configs

--- serie-service/src/main/java/com/ramv/serieservice/SerieServiceApplication.java
+++ serie-service/src/main/java/com/ramv/serieservice/SerieServiceApplication.java
@@ -2,8 +2,11 @@ package com.ramv.serieservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SerieServiceApplication {
 
 	public static void main(String[] args) {
