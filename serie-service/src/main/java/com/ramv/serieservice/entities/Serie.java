1008696b00420762a5053d863d5bf484190f2a29
royalejandro09 <roymartinezv@gmail.com>
2022-10-10 20:50:30 -0500

    chages configs

--- serie-service/src/main/java/com/ramv/serieservice/entities/Serie.java
+++ serie-service/src/main/java/com/ramv/serieservice/entities/Serie.java
@@ -3,6 +3,7 @@ package com.ramv.serieservice.entities;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class Serie {
     private String id;
     private String name;
     private String genre;
+    @DBRef
     private List<Season> seasons;
 
     public static class Season {
@@ -26,6 +28,7 @@ public class Serie {
         @Id
         private String id;
         private int seasonNumber;
+        @DBRef
         private List<Chapter> chapters;
 
         public static class Chapter {
