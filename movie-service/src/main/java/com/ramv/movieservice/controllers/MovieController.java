a6bca9a99ddcc1e9fcc37be6c1415bc4fb5c394a
royalejandro09 <roymartinezv@gmail.com>
2023-01-02 22:01:57 -0500

    fix

--- movie-service/src/main/java/com/ramv/movieservice/controllers/MovieController.java
+++ movie-service/src/main/java/com/ramv/movieservice/controllers/MovieController.java
@@ -3,7 +3,6 @@ package com.ramv.movieservice.controllers;
 import com.ramv.movieservice.dtos.MovieDTO;
 import com.ramv.movieservice.services.IMovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.parser.Part;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,21 +22,18 @@ public class MovieController {
     @PostMapping
     public ResponseEntity<MovieDTO> saveMovie(@RequestBody MovieDTO movie) {
         MovieDTO movieResponse = movieService.saveMovie(movie);
-
         return ResponseEntity.ok(movieResponse);
     }
 
     @GetMapping("/{genre}")
     public ResponseEntity<List<MovieDTO>> findByGenre(@PathVariable String genre) {
         List<MovieDTO> movies = movieService.findByGenre(genre);
-
         return ResponseEntity.ok(movies);
     }
 
     @GetMapping
     public ResponseEntity<List<MovieDTO>> getAllMovies() {
         List<MovieDTO> movies = movieService.getAllMovies();
-
         return ResponseEntity.ok(movies);
     }
 }
--- serie-service/src/main/java/com/ramv/serieservice/SerieServiceApplication.java
+++ serie-service/src/main/java/com/ramv/serieservice/SerieServiceApplication.java
@@ -1,15 +1,13 @@
 package com.ramv.serieservice;
 
-import com.mongodb.MongoCredential;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
 @SpringBootApplication
 @EnableEurekaClient
+@EnableRabbit
 public class SerieServiceApplication {
 
     public static void main(String[] args) {
