c545a5e4b8212cdea07b197adb1fa30663e8e06a
royalejandro09 <roymartinezv@gmail.com>
2023-01-07 17:53:45 -0500

    make.sh

--- make.sh
+++ make.sh
@@ -1,2 +1,29 @@
+cd eureka-service/
+mvn clean && mvn package -DskipTests
+docker build . -t eureka-service
+cd ..
+
+cd config-service/
+mvn clean && mvn package -DskipTests
+docker build . -t config-service
+cd ..
+
+cd api-gateway/
+mvn clean && mvn package -DskipTests
+docker build . -t api-gateway
+cd ..
+
+cd catalog-service/
+mvn clean && mvn package -DskipTests
+docker build . -t catalog-service
+cd ..
+
+cd movie-service/
+mvn clean && mvn package -DskipTests
+docker build . -t movie-service
+cd ..
+
+cd serie-service/
 mvn clean && mvn package -DskipTests
 docker build . -t serie-service
+cd ..
