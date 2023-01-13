faa0229ffde86adc42b00be0e77e9f2eb2c3ed35
royalejandro09 <roymartinezv@gmail.com>
2023-01-12 21:47:23 -0500

    minior changes

--- catalog-service/src/main/java/com/ramv/catalogservice/service/impl/CatalogService.java
+++ catalog-service/src/main/java/com/ramv/catalogservice/service/impl/CatalogService.java
@@ -11,7 +11,6 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
