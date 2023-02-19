08ab2198cb5465eaa6879b543fdda8029cdf99f4
royalejandro09 <roymartinezv@gmail.com>
2023-02-19 12:25:36 -0500

    fixed path config-service in projects

--- serie-service/src/main/java/com/ramv/serieservice/SerieServiceApplication.java
+++ serie-service/src/main/java/com/ramv/serieservice/SerieServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 @EnableRabbit
 public class SerieServiceApplication {
 
