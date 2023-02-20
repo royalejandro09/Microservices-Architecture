b36d3e0550e841b6f7589d97110fc2c3062a9e54
royalejandro09 <roymartinezv@gmail.com>
2023-02-19 19:39:39 -0500

    fix config eureka

--- make.sh
+++ make.sh
@@ -1,31 +1,31 @@
 cd eureka-service/
-mvn clean && mvn package -DskipTests
+#mvn clean && mvn package -DskipTests
 docker build . -t eureka-service
 cd ..
 
 cd config-service/
-mvn clean && mvn package -DskipTests
+#mvn clean && mvn package -DskipTests
 docker build . -t config-service
 cd ..
 
 cd api-gateway/
-mvn clean && mvn package -DskipTests
+#mvn clean && mvn package -DskipTests
 docker build . -t api-gateway
 cd ..
 
 cd catalog-service/
-mvn clean && mvn package -DskipTests
+#mvn clean && mvn package -DskipTests
 docker build . -t catalog-service
 cd ..
 
 cd movie-service/
-mvn clean && mvn package -DskipTests
+#mvn clean && mvn package -DskipTests
 docker build . -t movie-service
 cd ..
 
 cd serie-service/
-mvn clean && mvn package -DskipTests
+#mvn clean && mvn package -DskipTests
 docker build . -t serie-service
 cd ..
 
-docker-compose up
+#docker-compose up
